f1ead5c7c5e560767c127228077af24df25ccb7b
sun <yyizi0913@gmail.com>
2023-08-24 00:48:35 +0900

    main 수정

--- script/index.js
+++ script/index.js
@@ -1,9 +1,15 @@
 // main_slide
+const bullet = ['말 놓을 용기','에고이스트']
+const txt = document.querySelectorAll('.txt p')
 const main_slide = new Swiper('#main_slide',{
     autoplay:{delay:2000},
     loop:true,
     speed:800,
-    scrollbar: {
-        el: '.swiper-scrollbar',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<div class="' + className + '"><span>' + (bullet[index]) + '</span></div>';
+            }
     },
 })
