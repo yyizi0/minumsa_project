90624aa063708f561f3627ad7159351e5860ee5f
yuseon <yyizi0913@gmail.com>
2023-08-24 18:12:02 +0900

    main 반응형

--- script/common.js
+++ script/common.js
@@ -2,7 +2,9 @@
 const nav = document.querySelectorAll('nav > ul > li')
 const nav_a = document.querySelectorAll('nav > ul > li > a')
 const sub = document.querySelectorAll('.sub')
-console.log(nav,sub,nav_a)
+const body_scroll = document.querySelector('body, html')
+const header_bg = document.querySelector('header')
+console.log(nav,sub,nav_a,body_scroll)
 // 1. 서브메뉴 안보이기
 // 2. 마우스 오버시 메뉴 보이기, active 추가
 // 3. 마우스 아웃시 메뉴 사라지게하기, active 제거
@@ -12,9 +14,19 @@ nav.forEach(function(t,i){
     t.addEventListener('mouseover',function(){
         sub[i].style.display = 'block'
         nav_a[i].classList.add('active')
+        header_bg.style.backgroundColor = '#fff'
     })
     t.addEventListener('mouseout',function(){
         sub[i].style.display = 'none'
         nav_a[i].classList.remove('active')
+        header_bg.style.backgroundColor = 'rgba(255,255,255,0)'
     })
+})
+window.addEventListener('scroll',function(){
+    if(window.scrollY > 0){
+        // console.log('.')
+        header_bg.style.backgroundColor = '#fff'
+    }else {
+        header_bg.style.backgroundColor = 'rgba(255,255,255,0)'
+    }
 })
--- script/index.js
+++ script/index.js
@@ -13,3 +13,31 @@ const main_slide = new Swiper('#main_slide',{
             }
     },
 })
+const sub_slide = new Swiper('#sub_slide',{
+    loop:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+})
+// new_book
+const tab_title = document.querySelectorAll('#new_book .tab_title button')
+const tab_contents = document.querySelectorAll('#new_book .tab_contents .contents')
+console.log(tab_title,tab_contents)
+for(let i of tab_contents){i.style.display = 'none'}
+for(let i of tab_title){i.classList.remove('active')}
+tab_title[0].classList.add('active')
+tab_contents[0].style.display = 'flex'
+tab_title.forEach(function(t,i,a){
+    t.addEventListener('click',function(){
+        for(let j of tab_contents){j.style.display = 'none'}
+        tab_contents[i].style.display = 'flex'
+        for(let u of tab_title){u.classList.remove('active')}
+        t.classList.add('active')
+    })
+    
+})
