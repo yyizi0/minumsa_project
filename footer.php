7a2fbb3d6706d757eb686f2f0c5f9ca038f2cea1
yuseon <yyizi0913@gmail.com>
2023-08-28 12:06:51 +0900

    반응형, php 작업

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <div class="top">
+        <a href="./index.html"><img src="./images/minumsa_white.png" alt="민음사"></a>
+        <div class="sns">
+            <a href="#"><img src="./images/sns_instagram.png" alt="인스타그램"></a>
+            <a href="#"><img src="./images/sns_blog.png" alt="블로그"></a>
+            <a href="#"><img src="./images/sns_youtube.png" alt="유튜브"></a>
+            <a href="#"><img src="./images/sns_twitter.png" alt="트위터"></a>
+            <a href="#"><img src="./images/sns_facebook.png" alt="페이스북"></a>
+        </div>
+    </div>
+    <div class="btm">
+        <div class="left">
+            <div class="fnb">
+                <a href="#">회원약관</a>
+                <a href="#">개인정보처리방침</a>
+                <a href="#">문의</a>
+                <a href="#">2차 저작권 문의 가이드</a>
+                <a href="#">위치 및 연락처</a>
+                <a href="#">사이트맵</a>
+                <a href="#">소식 RSS</a>
+                <a href="#">민음사 도서 RSS</a>
+            </div>
+            <address>
+                (주)민음북스 | 대표이사: 박상준 | 사업자등록번호: 211-88-41803 | 통신판매업 신고번호: 제2010-서울강남-00971호<br>
+                주소: 서울시 강남구 신사동 506 강남출판문화센터 5층 | 새주소: 서울시 강남구 도산대로 1길 62 5층<br>
+                전화: 02-515-2000 | webmaster@minumsa.com
+            </address>
+        </div>
+        <div class="site">
+            <select name="site" id="site">
+                <option value="minumsa_brand">민음사 출판그룹</option>
+                <option value="minumsa">민음사</option>
+                <option value="goldenbough">황금가지</option>
+                <option value="sciencebooks">사이언스북스</option>
+                <option value="semicolon">세미콜론</option>
+                <option value="minumin">민음인</option>
+                <option value="panmidong">판미동</option>
+                <option value="banbi">반비</option>
+                <option value="pulp">펄프</option>
+                <option value="bir">비룡소</option>
+            </select>
+        </div>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,137 @@
+<header>
+    <div class="left">
+        <h1><a href="./index.html"><img src="./images/minumsa.png" alt="민음사"></a></h1>
+        <nav>
+            <ul>
+                <li><a href="#" class="active">브랜드</a>
+                    <ul class="sub">
+                        <li><a href="#">브랜드 소개</a></li>
+                        <li><a href="#">인사말</a></li>
+                        <li><a href="#">역사</a></li>
+                        <li><a href="#">채용정보</a></li>
+                        <li><a href="#">거래처 안내</a></li>
+                        <li><a href="#">찾아오시는 길</a></li>
+                    </ul>
+                </li>
+                <li><a href="./book_list.html">도서</a>
+                    <ul class="sub">
+                        <li><a href="./book_list.html">전체 도서</a></li>
+                        <li><a href="#">카테고리별 도서</a></li>
+                        <li><a href="#">전자책</a></li>
+                        <li><a href="#">리뷰</a></li>
+                        <li><a href="#">도서 · 작가 DB</a></li>
+                        <li><a href="#">도서목록 다운로드</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">문학상</a>
+                    <ul class="sub">
+                        <li><a href="#">오늘의 작가상</a></li>
+                        <li><a href="#">김수영 문학상</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">북클럽 · 정기구독</a>
+                    <ul class="sub">
+                        <li><a href="#">민음사 멤버십</a></li>
+                        <li><a href="#">민음북클럽</a></li>
+                        <li><a href="#">릿터 · 한편 정기구독</a></li>
+                        <li><a href="#">민음북샵</a></li>
+                        <li><a href="#">민음사 패밀리데이</a></li>
+                        <li><a href="#">동네서점 Event</a></li>
+                        <li><a href="#">추천 북로그 & 북캐스트</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">커뮤니티</a>
+                    <ul class="sub">
+                        <li><a href="#">민음커뮤니티</a></li>
+                        <li><a href="#">독서모임 캘린더</a></li>
+                        <li><a href="#">연재</a></li>
+                        <li><a href="#">책의 사생활</a></li>
+                        <li><a href="#">원고투고</a></li>
+                        <li><a href="#">1:1 문의</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">공지사항 · 이벤트</a>
+                    <ul class="sub">
+                        <li><a href="#">공지사항 · 이벤트</a></li>
+                        <li><a href="#">e-스탬프 이벤트</a></li>
+                        <li><a href="#">자료실</a></li>
+                        <li><a href="#">보도자료</a></li>
+                        <li><a href="#">2차 저작권 가이드</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="user_menu">
+        <a href="#" class="lang">KR</a>
+        <a href="#" class="login"><img src="./images/icon_user.png" alt="로그인"></a>
+        <a href="#" class="search"><img src="./images/icon_search.png" alt="검색"></a>
+        <!-- tablet, moblie menu -->
+        <a href="#" class="open_nav">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+        <!-- <button id="close" type="button"><img src="./images/icon_close.png" alt="모바일 메뉴 닫기"></button> -->
+    </div>
+    <div class="m_nav_open active">
+        <ul>
+            <li><a href="#">브랜드</a>
+                <ul class="sub active">
+                    <li><a href="#">브랜드 소개</a></li>
+                    <li><a href="#">인사말</a></li>
+                    <li><a href="#">역사</a></li>
+                    <li><a href="#">채용정보</a></li>
+                    <li><a href="#">거래처 안내</a></li>
+                    <li><a href="#">찾아오시는 길</a></li>
+                </ul>
+            </li>
+            <li><a href="#">도서</a>
+                <ul class="sub">
+                    <li><a href="./book_list.html">전체 도서</a></li>
+                    <li><a href="#">카테고리별 도서</a></li>
+                    <li><a href="#">전자책</a></li>
+                    <li><a href="#">리뷰</a></li>
+                    <li><a href="#">도서 · 작가 DB</a></li>
+                    <li><a href="#">도서목록 다운로드</a></li>
+                </ul>
+            </li>
+            <li><a href="#">문학상</a>
+                <ul class="sub">
+                    <li><a href="#">오늘의 작가상</a></li>
+                    <li><a href="#">김수영 문학상</a></li>
+                </ul>
+            </li>
+            <li><a href="#">북클럽 · 정기구독</a>
+                <ul class="sub">
+                    <li><a href="#">민음사 멤버십</a></li>
+                    <li><a href="#">민음북클럽</a></li>
+                    <li><a href="#">릿터 · 한편 정기구독</a></li>
+                    <li><a href="#">민음북샵</a></li>
+                    <li><a href="#">민음사 패밀리데이</a></li>
+                    <li><a href="#">동네서점 Event</a></li>
+                    <li><a href="#">추천 북로그 & 북캐스트</a></li>
+                </ul>
+            </li>
+            <li><a href="#">커뮤니티</a>
+                <ul class="sub">
+                    <li><a href="#">민음커뮤니티</a></li>
+                    <li><a href="#">독서모임 캘린더</a></li>
+                    <li><a href="#">연재</a></li>
+                    <li><a href="#">책의 사생활</a></li>
+                    <li><a href="#">원고투고</a></li>
+                    <li><a href="#">1:1 문의</a></li>
+                </ul>
+            </li>
+            <li><a href="#">공지사항 · 이벤트</a>
+                <ul class="sub">
+                    <li><a href="#">공지사항 · 이벤트</a></li>
+                    <li><a href="#">e-스탬프 이벤트</a></li>
+                    <li><a href="#">자료실</a></li>
+                    <li><a href="#">보도자료</a></li>
+                    <li><a href="#">2차 저작권 가이드</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</header>
